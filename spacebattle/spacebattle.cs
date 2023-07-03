7a322fbf58548da8639396904889f6130b8b79e1
Zadorozhnyi <sz1253265@gmail.com>
2023-07-03 13:07:03 +0000

    добавление топлива и поворота

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -10,4 +10,22 @@ public class SpaceShip
         double[] total = {Position[0] + Speed[0], Position[1] + Speed[1]};
         return total;
     }
+ 
+    public static double Fuel (double fuel, double change_fuel){
+        if (fuel < change_fuel){
+            throw new Exception();
+        }
+        double total = fuel - change_fuel;
+        return total;
+    }
+
+
+    public static double Turn (double corner, double change_angle,
+     bool isKnowCorner, bool isKnowChangeAngle, bool isCanChangeAngle){
+        if (!isKnowCorner || !isKnowChangeAngle || !isCanChangeAngle){
+            throw new Exception();
+        }
+        double total = corner + change_angle;
+        return total;
+    }
 }
